34bb4b2e009751c450aee2adb43cec3020605b07
CrashSystem <126931250+BymBu@users.noreply.github.com>
2026-07-19 10:02:57 +0800

    chore: верификация яндекс и гугл

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -24,6 +24,10 @@ export const metadata = {
     "Село Зырянск",
     "Село Зырянск история",
   ],
+  verification: {
+    yandex: "06e4f2fb3b10719a",
+    google: "UTUYV47UoZeSSba1qc7PaJYiKIpIBy1SnbA6raqDUvo",
+  },
   openGraph: {
     title: "Зырянск: Карта Памяти",
     description: "Исчезающая история села глазами местного жителя.",
